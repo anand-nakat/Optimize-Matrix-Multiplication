d5b79c0cc3042e5b12d867ca72e8393ba8b7f972
anand-nakat <nakatanand2000@gmail.com>
2022-11-19 00:35:00 +0530

    SingleThread Optmized: Tiled MM

--- PartA/header/single_thread.h
+++ PartA/header/single_thread.h
@@ -3,25 +3,33 @@
 void singleThread(int N, int *matA, int *matB, int *output)
 {
   assert(N >= 4 and N == (N & ~(N - 1)));
-  for (int rowA = 0; rowA < N; rowA += 2)
-  {
-    for (int colB = 0; colB < N; colB += 2)
-    {
-      int sum = 0;
-      /*Iterate for Row*/
-      for (int iter = 0; iter < N; iter++)
-      {
-        sum += matA[rowA * N + iter] * matB[iter * N + colB];
-        sum += matA[rowA * N + iter] * matB[iter * N + (colB + 1)];
-        sum += matA[(rowA + 1) * N + iter] * matB[iter * N + colB];
-        sum += matA[(rowA + 1) * N + iter] * matB[iter * N + (colB + 1)];
-      }
 
-      // compute output indices
-      int rowC = rowA >> 1;
-      int colC = colB >> 1;
-      int indexC = rowC * (N >> 1) + colC;
-      output[indexC] = sum;
+  int t = 16; // Tile-Size
+  for (int i = 0; i < N; i += t)
+  {
+    for (int k = 0; k < N; k += t)
+    {
+      for (int j = 0; j < N; j += t)
+      {
+        for (int rowA = i; rowA < i + t; rowA += 2)
+        {
+          for (int colB = j; colB < j + t; colB += 2)
+          {
+            int sum = 0;
+            int rowC = rowA >> 1;
+            int colC = colB >> 1;
+            int indexC = rowC * (N >> 1) + colC;
+
+            for (int iter = k; iter < k + t; iter++)
+            {
+              output[indexC] += matA[rowA * N + iter] * matB[iter * N + colB];
+              output[indexC] += matA[rowA * N + iter] * matB[iter * N + (colB + 1)];
+              output[indexC] += matA[(rowA + 1) * N + iter] * matB[iter * N + colB];
+              output[indexC] += matA[(rowA + 1) * N + iter] * matB[iter * N + (colB + 1)];
+            }
+          }
+        }
+      }
     }
   }
 }
--- PartA/main.cpp
+++ PartA/main.cpp
@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
 
   // Untimed, warmup caches and TLB
   int *output_reference = new int[(N >> 1) * (N >> 1)];
-  reference(N, matA, matB, output_reference);
+  // reference(N, matA, matB, output_reference);
 
   // Execute reference program
   auto begin = TIME_NOW;
@@ -115,6 +115,19 @@ int main(int argc, char *argv[])
   end = TIME_NOW;
   cout << "Single thread execution time: " << (double)TIME_DIFF(std::chrono::microseconds, begin, end) / 1000.0 << " ms\n";
 
+  if (N <= 16)
+  {
+    cerr << "\nOutput: " << endl;
+    for (int i = 0; i < (N >> 1); i++)
+    {
+      for (int j = 0; j < (N >> 1); j++)
+      {
+        cerr << output_single[i * (N >> 1) + j] << "\t";
+      }
+      cerr << endl;
+    }
+  }
+
   for (int i = 0; i < ((N >> 1) * (N >> 1)); ++i)
     if (output_single[i] != output_reference[i])
     {
@@ -125,7 +138,7 @@ int main(int argc, char *argv[])
   // Execute multi-thread
   int *output_multi = new int[(N >> 1) * (N >> 1)];
   begin = TIME_NOW;
-  multiThread(N, matA, matB, output_multi);
+  // multiThread(N, matA, matB, output_multi);
   end = TIME_NOW;
   cout << "Multi-threaded execution time: " << (double)TIME_DIFF(std::chrono::microseconds, begin, end) / 1000.0 << " ms\n";
 

6a250384e9008362f4843673f5f244c44f6f7f63
anand-nakat <nakatanand2000@gmail.com>
2022-11-30 16:23:26 +0530

    GPU Implemented

--- PartB/header/gpu_thread.h
+++ PartB/header/gpu_thread.h
@@ -1,6 +1,86 @@
 // Create other necessary functions here
+__global__ void GPU_Mul(int MatSize, int *matA, int *matB, int *matC)
+{
+
+    // Caclulating Global rows and coloumn for each thread
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Calculating row and column for reduced matrice multiplication
+    int R1 = 2 * row;
+    int R2 = 2 * row + 1;
+    int C1 = 2 * col;
+    int C2 = 2 * col + 1;
+
+    int tempMag = MatSize / 2;
+    int tempValue = 0;
+
+    for (int k = 0; k < MatSize; k++)
+    {
+
+        // Sum1 ----> Calculating dot product of (R1,C1)
+        int posA1 = matA[R1 * MatSize + k];
+        int posB1 = matB[k * MatSize + C1];
+        tempValue += posA1 * posB1;
+
+        // Sum2 ----> Calculating dot product of (R1,C2)
+        int posA2 = matA[R1 * MatSize + k];
+        int posB2 = matB[k * MatSize + C2];
+        tempValue += posA2 * posB2;
+
+        // S3 ----> Calculating dot product of (R2,C1)
+        int posA3 = matA[R2 * MatSize + k];
+        int posB3 = matB[k * MatSize + C1];
+        tempValue += posA3 * posB3;
+
+        // Sum4 ----> Calculating dot product of (R2,C2)
+        int posA4 = matA[R2 * MatSize + k];
+        int posB4 = matB[k * MatSize + C2];
+        tempValue += posA4 * posB4;
+    }
+    matC[row * tempMag + col] = tempValue;
+}
 
 // Fill in this function
 void gpuThread(int N, int *matA, int *matB, int *output)
 {
+    // cout<<"Thread Executing"<<"\n";
+
+    // Calculate size of matrix in bytes
+    size_t size = N * N * sizeof(int);
+
+    // Pointers for device matrices
+    int *A, *B, *C;
+
+    // Allocate device memory for matrice A
+    cudaMalloc((void **)&A, size);
+    cudaMemcpy(A, matA, size, cudaMemcpyHostToDevice);
+
+    // Allocate device memory for matrice B
+    cudaMalloc((void **)&B, size);
+    cudaMemcpy(B, matB, size, cudaMemcpyHostToDevice);
+
+    // Allocate device memory for d_C
+    cudaMalloc((void **)&C, size / 4);
+
+    // Launch kernel on device
+    int blocks = 16;
+    int threads = (N / (2 * blocks));
+    dim3 gridDim(threads, threads);
+    dim3 blockDim(blocks, blocks);
+
+    // Invoking Kernel
+    GPU_Mul<<<gridDim, blockDim>>>(N, A, B, C);
+
+    // Copy result from device to host
+    cudaMemcpy(output, C, size / 4, cudaMemcpyDeviceToHost);
+
+    // Free device memory
+    cudaFree(A);
+    cudaFree(B);
+    cudaFree(C);
+
+    // Free host memory
+    cudaFree(matA);
+    cudaFree(matB);
 }
--- PartB/main.cu
+++ PartB/main.cu
@@ -7,28 +7,32 @@
 #include <assert.h>
 using namespace std;
 
+#define TIME_NOW std::chrono::high_resolution_clock::now()
+#define TIME_DIFF(gran, start, end) std::chrono::duration_cast<gran>(end - start).count()
 #include "gpu_thread.h"
 
 // Used to cross-check answer. DO NOT MODIFY!
 void reference(int N, int *matA, int *matB, int *output)
 {
   // enforce N to be power of 2 and greater than 2
-  assert( N>=4 and N == ( N &~ (N-1)));
-  for(int rowA = 0; rowA < N; rowA +=2) {
-    for(int colB = 0; colB < N; colB += 2){
+  // assert( N>=4 and N == ( N &~ (N-1)));
+  for (int rowA = 0; rowA < N; rowA += 2)
+  {
+    for (int colB = 0; colB < N; colB += 2)
+    {
       int sum = 0;
-      for(int iter = 0; iter < N; iter++) 
+      for (int iter = 0; iter < N; iter++)
       {
         sum += matA[rowA * N + iter] * matB[iter * N + colB];
-        sum += matA[(rowA+1) * N + iter] * matB[iter * N + colB];
-        sum += matA[rowA * N + iter] * matB[iter * N + (colB+1)];
-        sum += matA[(rowA+1) * N + iter] * matB[iter * N + (colB+1)];
+        sum += matA[(rowA + 1) * N + iter] * matB[iter * N + colB];
+        sum += matA[rowA * N + iter] * matB[iter * N + (colB + 1)];
+        sum += matA[(rowA + 1) * N + iter] * matB[iter * N + (colB + 1)];
       }
 
       // compute output indices
-      int rowC = rowA>>1;
-      int colC = colB>>1;
-      int indexC = rowC * (N>>1) + colC;
+      int rowC = rowA >> 1;
+      int colC = colB >> 1;
+      int indexC = rowC * (N >> 1) + colC;
       output[indexC] = sum;
     }
   }
@@ -36,43 +40,59 @@ void reference(int N, int *matA, int *matB, int *output)
 
 int main(int argc, char *argv[])
 {
-    // Input size of square matrices// Input size of square matrices
-    int N;
-    string file_name; 
-    if (argc < 2) 
-        file_name = "data/input_128.in"; 
-    else 
-        file_name = argv[1]; 
-    ifstream input_file; 
-    input_file.open(file_name); 
-    input_file >> N;
-    cout << "Input matrix of size " << N << "\n";
-    
-    // Input matrix A
-    int *matA = new int[N * N];
-    for(int i = 0; i < N; ++i)
-        for(int j = 0; j < N; ++j)
-            input_file >> matA[i * N + j];
+  // Input size of square matrices// Input size of square matrices
+  int N;
+  string file_name;
+  if (argc < 2)
+    file_name = "data/input_128.in";
+  else
+    file_name = argv[1];
+  ifstream input_file;
+  input_file.open(file_name);
+  input_file >> N;
+  cout << "Input matrix of size " << N << "\n";
 
-    // Input matrix B
-    int *matB = new int[N * N];
-    for(int i = 0; i < N; ++i)
-        for(int j = 0; j < N; ++j)
-            input_file >> matB[i * N + j];
-    
-    // Execute reference program
-    int *output_reference = new int[N*(N>>1)];
-    reference(N, matA, matB, output_reference);
-    
-    // Execute gpu version
-    int *output_gpu = new int[N*(N>>1)];
-    gpuThread(N, matA, matB, output_gpu);
-    
-    for(int i = 0; i < N*(N>>1); ++i)
-        if(output_gpu[i] != output_reference[i]) {
-            cout << "Mismatch at " << i << "\n";
-            cout << "GPU output: " << output_gpu[i] << ", required output: " << output_reference[i] << "\n";
-            exit(0);
-        }
-    input_file.close(); 
+  // Input matrix A
+  int *matA = new int[N * N];
+  for (int i = 0; i < N; ++i)
+    for (int j = 0; j < N; ++j)
+      input_file >> matA[i * N + j];
+
+  // Input matrix B
+  int *matB = new int[N * N];
+  for (int i = 0; i < N; ++i)
+    for (int j = 0; j < N; ++j)
+      input_file >> matB[i * N + j];
+
+  cout << "Input taking complete"
+       << "\n";
+
+  // Execute reference program
+  auto begin = TIME_NOW;
+  int *output_reference = new int[N * (N >> 1)];
+  reference(N, matA, matB, output_reference);
+  auto end = TIME_NOW;
+  cout << "Reference Code done"
+       << "\n";
+  cout << "Reference execution time: " << (double)TIME_DIFF(std::chrono::microseconds, begin, end) / 1000.0 << " ms\n";
+
+  // Execute gpu version
+  int *output_gpu = new int[(N >> 1) * (N >> 1)];
+  begin = TIME_NOW;
+  gpuThread(N, matA, matB, output_gpu);
+  end = TIME_NOW;
+  cout << "Reference Code done"
+       << "\n";
+  cout << "GPU execution time: " << (double)TIME_DIFF(std::chrono::microseconds, begin, end) / 1000.0 << " ms\n";
+
+  cudaThreadSynchronize();
+
+  for (int i = 0; i < (N >> 1) * (N >> 1); ++i)
+    if (output_gpu[i] != output_reference[i])
+    {
+      cout << "Mismatch at " << i << "\n";
+      cout << "GPU output: " << output_gpu[i] << ", required output: " << output_reference[i] << "\n";
+      exit(0);
+    }
+  input_file.close();
 }
